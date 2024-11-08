578782a89842d3b951119bb32a7f77808cf1bf25
이동복 <typoon0820@gmail.com>
2024-11-08 15:21:44 +0900

    FEAT: #24 nfc-token-service 개발

---
+++ backend/nfc-token-service/src/main/kotlin/org/ssmartoffice/nfctokenservice/global/config/WebConfigure.kt
@@ -0,0 +1,17 @@
+package org.ssmartoffice.nfctokenservice.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfigure : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
