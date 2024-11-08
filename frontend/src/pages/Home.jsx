5dd57cc18f0e8f5080809a62ef0a1f2a40ad5b1d
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-08 16:40:42 +0900

    FEAT : #17 캘린더 UI 수정

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,17 +18,27 @@ const Home = () => {
       type: "WORK",
     },
     {
+      date: "2024-11-06",
+      name: "코드 리뷰 반영 및 수정3",
+      type: "WORK",
+    },
+    {
+      date: "2024-11-06",
+      name: "코드 리뷰 반영 및 수정4",
+      type: "WORK",
+    },
+    {
       date: "2024-11-01",
       name: "연차",
       type: "HOLIDAY",
     },
     {
-      date: "2024-11-29",
+      date: "2024-11-04",
       name: "회의",
       type: "MEETING",
     },
     {
-      date: "2024-11-30",
+      date: "2024-11-04",
       name: "코드 리뷰 반영 및 수정",
       type: "WORK",
     },
@@ -36,10 +46,54 @@ const Home = () => {
   const handleDateSelect = (date) => {
     setSelectedDate(date);
   };
+
+  // 출퇴근 정보
+  const attendanceData = [
+    {
+      attendanceId: 1,
+      userId: 1,
+      attendanceType: "START",
+      attendanceTime: "2024-11-06T09:40:00",
+    },
+    {
+      attendanceId: 2,
+      userId: 1,
+      attendanceType: "END",
+      attendanceTime: "2024-11-06T18:00:00",
+    },
+    {
+      attendanceId: 3,
+      userId: 1,
+      attendanceType: "START",
+      attendanceTime: "2024-11-01T09:00:00",
+    },
+    {
+      attendanceId: 4,
+      userId: 1,
+      attendanceType: "END",
+      attendanceTime: "2024-11-01T21:00:00",
+    },
+    {
+      attendanceId: 3,
+      userId: 1,
+      attendanceType: "START",
+      attendanceTime: "2024-11-04T09:00:00",
+    },
+    {
+      attendanceId: 4,
+      userId: 1,
+      attendanceType: "END",
+      attendanceTime: "2024-11-04T18:30:00",
+    },
+  ];
   return (
     <div className={styles.home}>
       <div className={styles.calendar}>
-        <MyCalendar monthData={monthData} onDateSelect={handleDateSelect} />
+        <MyCalendar
+          monthData={monthData}
+          attendanceData={attendanceData}
+          onDateSelect={handleDateSelect}
+        />
       </div>
       <div>
         <Todo selectedDate={selectedDate} monthData={monthData} />
