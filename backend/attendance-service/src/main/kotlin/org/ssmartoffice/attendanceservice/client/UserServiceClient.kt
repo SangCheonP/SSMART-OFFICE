e8ea20e273ddee9cd571ddfca5ce7e4cddf57c59
이동복 <typoon0820@gmail.com>
2024-11-12 09:59:43 +0900

    FIX: 프로젝트 명 및 properties 수정

---
+++ backend/attendance-service/src/main/kotlin/org/ssmartoffice/attendanceservice/client/UserServiceClient.kt
@@ -0,0 +1,15 @@
+package org.ssmartoffice.attendanceservice.client
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.ssmartoffice.attendanceservice.client.response.UserAuthenticationResponse
+import org.ssmartoffice.attendanceservice.global.dto.CommonResponse
+
+@FeignClient(name = "user-service")
+interface UserServiceClient {
+    @GetMapping("/api/v1/users/internal/authentication")
+    fun getIdAndRole(@RequestParam email: String): ResponseEntity<CommonResponse<UserAuthenticationResponse>>
+
+}
