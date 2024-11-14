bb7a675cc1b68a93a19f359b3100c49a915153d8
김세민 <2007ksm@naver.com>
2024-11-14 23:38:18 +0900

    FIX : #35 폰 정규 표현식 수정, 일정에 날짜 없던 버그 수정

--- backend/assignment-service/src/main/java/org/ssmartoffice/assignmentservice/controller/response/AssignmentInfoResponse.kt
+++ backend/assignment-service/src/main/java/org/ssmartoffice/assignmentservice/controller/response/AssignmentInfoResponse.kt
@@ -3,6 +3,7 @@ package org.ssmartoffice.assignmentservice.controller.response
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.ssmartoffice.assignmentservice.domain.Assignment
 import org.ssmartoffice.assignmentservice.domain.AssignmentType
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -15,23 +16,21 @@ data class AssignmentInfoResponse(
     val type: AssignmentType,
     val completed: Boolean = false,
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    val date: String? = null
+    val date: LocalDate? = null
 ) {
     companion object {
         private val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
 
         fun fromModel(
             assignment: Assignment,
-            includeDescription: Boolean = true,
-            includeDate: Boolean = false
         ): AssignmentInfoResponse {
             return AssignmentInfoResponse(
                 id = assignment.id,
                 name = assignment.name,
-                description = if (includeDescription) assignment.description else null,
+                description = assignment.description,
                 type = assignment.type,
                 completed = assignment.completed,
-                date = if (includeDate) assignment.date else null
+                date = assignment.date?.let { LocalDate.parse(it, formatter) }
             )
         }
     }
--- backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/controller/request/UserUpdateRequest.kt
+++ backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/controller/request/UserUpdateRequest.kt
@@ -8,6 +8,6 @@ data class UserUpdateRequest(
     val duty: String? = null,
     @field:Pattern(message = "유효한 URL 형식이 아닙니다.", regexp = "(http|https)://[a-zA-Z0-9./]+")
     val profileImageUrl: String? = null,
-    @field:Pattern(message = "유효한 전화번호 형식이 아닙니다.", regexp = "^\\\\d{3}-\\\\d{4}-\\\\d{4}\$")
+    @field:Pattern(message = "유효한 전화번호 형식이 아닙니다.", regexp = "^\\d{3}-\\d{4}-\\d{4}$")
     val phoneNumber: String? = null,
 )
