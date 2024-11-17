1f43a523850ab8316d97efca76e39ce4801b9e95
이동복 <typoon0820@gmail.com>
2024-11-17 10:30:55 +0900

    FEAT: #38 채팅 기능 추가 구현

--- backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/controller/port/ChatService.kt
+++ backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/controller/port/ChatService.kt
@@ -1,11 +1,15 @@
 package org.ssmartoffice.chatservice.controller.port
 
+import org.ssmartoffice.chatservice.controller.response.GetChatRoomResponse
+import org.ssmartoffice.chatservice.controller.response.MessageResponse
 import org.ssmartoffice.chatservice.domain.ChatRoom
 import org.ssmartoffice.chatservice.domain.Message
+import org.ssmartoffice.chatservice.domain.UserChatRoom
 
 interface ChatService {
-    fun saveMessage(message :Message)
+    fun saveMessage(message: Message)
     fun saveChatRoom(currentUserId: Long, userId: Long): Long
-
-    fun findChatroom(roomId: Long): ChatRoom
+    fun findUserChatroom(userId: Long, roomId: Long): UserChatRoom?
+    fun findMyChatrooms(userId: Long): List<GetChatRoomResponse>?
+    fun findMessagesByChatroomId(roomId: Long): List<MessageResponse>?
 }
